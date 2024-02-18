ade851e7b24ac91d6071c88d4d9b10bdca67d87d
gottbrecht <simo79j5@stud.kea.dk>
2024-02-18 10:17:37 +0100

    add method

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ const node1 = { //a node not depending on other nodes containing an "A"
     next: null,
     data: "A" //payload
 }
+const node2 = {
+    prev: null, 
+    next: null,
+    data:"B"
+}
+
+const node3 = {
+    prev: null,
+    next: null,
+    data: "E"
+}
+
+node1.next = node2;
+node2.prev = node1;
+node2.next = node3;
+node3.prev = node2;
+
+const linkedList = [node1, node2, node3];
+
+//Note: you cannot add notes and their connections at the same time. First when they exists
+
+
+
+const num = new LinkedList();
+
+
 
 //Make a class for your list. Put the single node in as a single ekement for the list - both head and tail 
 
@@ -35,32 +61,20 @@ class LinkedList {
         }
     }
 
+    add(node1) { //adds a new node in the end of the list
+        if(!this.head) {
+            this.head = node;
+            this.tail = node;
+    } else {
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+
+    } //this methid checks if the list is empty
+
 }
 
 //make an instance of LinkedList so you have code for testing. You can write that in the end of your JS file. 
 
 //make to more nodes og chain them together - hardcoded
-const node2 = {
-    prev: null, 
-    next: null,
-    data:"B"
 }
-
-const node3 = {
-    prev: null,
-    next: null,
-    data: "E"
-}
-
-node1.next = node2;
-node2.prev = node1;
-node2.next = node3;
-node3.prev = node2;
-
-
-//Note: you cannot add notes and their connections at the same time. First when they exists
-
-
-
-const num = new LinkedList();
-console.log(11);
